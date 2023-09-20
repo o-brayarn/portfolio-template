c588ca0acbe7067d0c4d80bbf71919ab8046ceb2
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-09-20 13:20:24 +0300

    create homepage files

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="profile__image"></div>
+      <div className="profile__summary"></div>
+    </>
+  );
+};
+
+export default Home;
