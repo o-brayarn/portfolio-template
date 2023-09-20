9150eb681c19b2f691df9dcde8d0a2bf9cf881b0
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-09-20 21:51:20 +0300

    complete homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import "./home.scss";
+import profile from "../../assets/profile.svg";
 
 const Home = () => {
   return (
-    <>
-      <div className="profile__image"></div>
-      <div className="profile__summary"></div>
-    </>
+    <section className="portfolio__summary">
+      <div className="profile__image">
+        <img src={profile} alt="An awesome person is chilling" />
+      </div>
+      <div className="profile__summary">
+        <h3>HELLO THERE, I AM:</h3>
+        <h1>OB Ogeda</h1>
+        <p>
+          I design, create, renovate, and maintain modern websites and
+          applications. I solve all your business problems.
+        </p>
+        <input type="button" value="Lets Talk" />
+      </div>
+    </section>
   );
 };
 
